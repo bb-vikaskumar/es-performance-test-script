eb6efcca6965b3b6aacc140329c9932670f0430c
Vikas Kumar <vikas.kumar16@bigbasket.com>
2022-03-23 16:58:01 +0530

    updated

--- experiment-3/index.js
+++ experiment-3/index.js
@@ -13,22 +13,24 @@ let elasticsearch = require('elasticsearch');
 let _ = require('lodash');
 const fs = require('fs')
   
-const ES_INDEX_NAME = "camp_info_v1";
+const ES_INDEX_NAME = "test_segments";
+const ES_INDEX_NAME_WITH_MID = "test_segments_with_mid";
 const ELASTICSEARCH_IP = "https://vpc-es-benchmarking-test-tg4mvjtk2uzeba4wvby3hanfy4.us-east-1.es.amazonaws.com";
 const ELASTICSEARCH_PORT = 443;
 
 const ACTIVITY_TYPE = 'create'; // create, fetch
-const TOTAL_DOCS_COUNT = 1;
+const TOTAL_DOCS_COUNT = 10;
 const FETCH_TYPE='count'  // time, count
 const BATCH_SIZE = 1;
 const SEARCH_DURATION_IN_MINS = 0.01;
-const WITH_MID = false;
-const MIDS_COUNT_PER_DOC = 10;
+const WITH_MID = true;
+const SPLIT_DOCS_ON_MIDS = 6;
+const MIDS_COUNT_PER_DOC = 20;
 const MIDS_SPACE_COUNT = MIDS_COUNT_PER_DOC * 30;
-const MIDS_DOCUMENTS_PERCENT = 70;
+const MIDS_DOCUMENTS_PERCENT = 100;
 const LOG_MOD = 500; // every LOD_MOD record will get logged
 const SEIGE_URLS_TO_KEEP = 3000;
-const ES_INDEX_FINAL = ES_INDEX_NAME;
+const ES_INDEX_FINAL = WITH_MID ? ES_INDEX_NAME_WITH_MID : ES_INDEX_NAME;
 
 let savedRecordFile = `./saved_docs.json`;
 
@@ -225,7 +227,6 @@ function generatePermutedDoc({sourceSpace, addMid}) {
     if(addMid) {
         doc['mid'] = generateCombination({list: sourceSpace.mid, size: MIDS_COUNT_PER_DOC})
     }
-    console.log('* c_sku_id len: ', doc.c_sku_id.length)
     return doc;
 }
 
@@ -252,7 +253,7 @@ function demoPromise({returnVal, delay}) {
     });
 }
 
-function processBatch({docs, from, batchId, batchSize, esIndex=ES_INDEX_NAME}) {
+function processBatch({docs, from, batchId, batchSize, esIndex=ES_INDEX_NAME, sameParent=false}) {
     let totalDocsCount = docs.length;
     from = from % totalDocsCount;
     let to = from+batchSize;
@@ -261,9 +262,9 @@ function processBatch({docs, from, batchId, batchSize, esIndex=ES_INDEX_NAME}) {
         batchDocs = batchDocs.concat(docs.slice(0, to-totalDocsCount))
     }
     return Promise.all(_.map(batchDocs, (doc, docIndex) => {
-        let docId = (batchId * batchSize) + docIndex;
+        let docId = sameParent ? `${batchId}_${docIndex}` : (batchId * batchSize) + docIndex;
         if(ACTIVITY_TYPE === 'create') {
-            console.log('write on ', esIndex, ' : ', JSON.stringify(doc));
+            console.log(docId, ' write on ', esIndex, ' : ', JSON.stringify(doc));
             return demoPromise({returnVal: 'processed '+ docId+ '_'+ batchId, delay: 2000});
             return ElasticSearchConnector.addDocument(esIndex, docId, doc);
         } else if(ACTIVITY_TYPE === 'fetch') {
@@ -413,10 +414,73 @@ async function createRecords({docCount, batchSize}) {
     console.log(`Generating records from space...`);
     
     let allDocs = generatePermutedDocs({sourceSpace: sourceSpace, count: docCount});
+    let allDocsWithMids = [];
     console.log(`Record generation success.`);
 
     let startTime = new Date();
-    processAllBatches({docs: allDocs, from: 0, batchId: 0, batchSize: batchSize})
+    if(WITH_MID) {
+        let batchId;
+        let batchSize = 1;
+        let totalDocsCount = TOTAL_DOCS_COUNT;
+
+        try {
+            for(let i=0; i<docCount; i++) {
+                let docsWithMids = [allDocs[i]];
+                let midCount = MIDS_COUNT_PER_DOC;
+                let mids = generateMids({sourceSpace: sourceSpace, size: midCount, addAll: (i%2 === 0)});
+                let docsToBeCreated = Math.ceil(mids.length/SPLIT_DOCS_ON_MIDS);
+
+                console.log('\n** docsToBeCreated ', docsToBeCreated, '\n')
+                let docToCopy = docsWithMids.pop();
+                for(let j=0; j<docsToBeCreated; j++) {
+                    let clonedDoc = _.cloneDeep(docToCopy);
+                    clonedDoc['mid'] = mids.slice(j*SPLIT_DOCS_ON_MIDS, (j+1)*SPLIT_DOCS_ON_MIDS);
+                    docsWithMids.push(clonedDoc); 
+                    allDocsWithMids.push(clonedDoc)
+                }
+                
+                console.log(`[${i}] Record generation success (with ${midCount} MID).`);
+                
+                batchId = i;
+                let from = 0;
+                let batchSize = docsWithMids.length;
+                let startTime = new Date();
+    
+                let processedAck = await processBatch({docs: docsWithMids, from, batchId, batchSize, esIndex: ES_INDEX_NAME_WITH_MID, sameParent: docsWithMids.length > 1});
+    
+                if(batchId % LOG_MOD === 0) {
+                    batchResults[batchId] = {
+                        timeElapsed: `${new Date() - startTime}ms`,
+                        totalDocsCount: totalDocsCount,
+                        from: from,
+                        batchSize: batchSize                     
+                    }
+                    console.log(`[success] batch: ${batchId} | ${new Date() - startTime}ms`);
+                }
+
+                allDocsWithMids.map(docWithMid => {
+                    if(docWithMid['mid'] && docWithMid['mid'].length > 4) {
+                        docWithMid['mid'] = docWithMid['mid'].slice(1, 4);
+                    }
+                })
+            }
+            // Write data in 'Output.txt' .
+            fs.writeFile(`saved_docs.json`, JSON.stringify({records: allDocsWithMids}), (err) => {
+                if (err) throw err;
+                console.log(`Records(${allDocsWithMids.length}) written to saved_docs.json`);
+            })
+                
+        } catch(e) {
+            batchResults[batchId] = {
+                error: e.stack,
+                totalDocsCount: totalDocsCount,
+                batchId: batchId,
+                batchSize: batchSize                     
+            }
+            console.log(`[error] batch (with mid): ${batchId} | ${new Date() - startTime}ms, error: ${e.stack}`);
+        }
+    } else {
+        processAllBatches({docs: allDocs, from: 0, batchId: 0, batchSize: batchSize})
         .then((res) => { 
             console.log({batchResults: batchResults, totalWriteTime: `${new Date() - startTime}ms`});
             // Write data in 'Output.txt' .
@@ -425,65 +489,10 @@ async function createRecords({docCount, batchSize}) {
                 console.log(`Records written to saved_docs.json`);
             })
         })
-        .then(async (res2) => {
-            if(WITH_MID) {
-                let batchId;
-                let batchSize = 1;
-                let totalDocsCount = TOTAL_DOCS_COUNT;
-        
-                try {
-                    for(let i=0; i<docCount; i++) {
-                        let addMid = (i*100/totalDocsCount) < MIDS_DOCUMENTS_PERCENT;
-                        let docWithMid = allDocs[i];
-                        let midCount = (i%100 == 0) ? (MIDS_COUNT_PER_DOC*5) : MIDS_COUNT_PER_DOC;
-                        if(addMid) {
-                            docWithMid['mid'] = generateMids({sourceSpace: sourceSpace, size: midCount, addAll: (i%2 === 0)});
-                        } else {
-                            docWithMid['mid'] = ['all'];
-                        }
-                        
-                        console.log(`[${i}] Record generation success (with ${midCount} MID).`);
-                        
-                        batchId = i;
-                        let from = 0;
-                        let batchSize = 1;
-                        let startTime = new Date();
-            
-                        let processedAck = await processBatch({docs: [docWithMid], from, batchId, batchSize, esIndex: ES_INDEX_NAME_WITH_MID});
-            
-                        if(batchId % LOG_MOD === 0) {
-                            batchResults[batchId] = {
-                                timeElapsed: `${new Date() - startTime}ms`,
-                                totalDocsCount: totalDocsCount,
-                                from: from,
-                                batchSize: batchSize                     
-                            }
-                            console.log(`[success] batch: ${batchId} | ${new Date() - startTime}ms`);
-                        }
-
-                        
-                        allDocs[i]['mid'] = docWithMid['mid'].slice(1, 4);
-                    }
-                    // Write data in 'Output.txt' .
-                    fs.writeFile(`saved_docs.json`, JSON.stringify({records: allDocs}), (err) => {
-                        if (err) throw err;
-                        console.log(`Records written to saved_docs.json`);
-                    })
-                        
-                } catch(e) {
-                    batchResults[batchId] = {
-                        error: e.stack,
-                        totalDocsCount: totalDocsCount,
-                        batchId: batchId,
-                        batchSize: batchSize                     
-                    }
-                    console.log(`[error] batch (with mid): ${batchId} | ${new Date() - startTime}ms, error: ${e.stack}`);
-                }
-            }
-        })
         .catch((e) => {
             console.log({batchResults: batchResults, totalWriteTime: `${new Date() - startTime}ms`, error: e});
         });
+    }
 }
 
 async function fetchRecords({docCount, batchSize}) {
@@ -525,15 +534,15 @@ async function fetchRecords({docCount, batchSize}) {
 
     let allQueries = generateFetchQueries({docs: allDocs, termsCount: termsCount});
     
-    let siegeUrls = [];
-    for(let query of allQueries.slice(0, SEIGE_URLS_TO_KEEP)) {
-        let siegeUrl = `${ELASTICSEARCH_IP}/${ES_INDEX_FINAL}/_search POST ${JSON.stringify(query)}`;
-        siegeUrls.push(siegeUrl);
-    }
+    // let siegeUrls = [];
+    // for(let query of allQueries.slice(0, SEIGE_URLS_TO_KEEP)) {
+    //     let siegeUrl = `${ELASTICSEARCH_IP}/${ES_INDEX_FINAL}/_search POST ${JSON.stringify(query)}`;
+    //     siegeUrls.push(siegeUrl);
+    // }
 
-    fs.writeFile(`siegeUrls${WITH_MID ? '_withMid': ''}.txt`, siegeUrls.join('\n'), (err) => {
-        if (err) throw err;
-    })
+    // fs.writeFile(`siegeUrls${WITH_MID ? '_withMid': ''}.txt`, siegeUrls.join('\n'), (err) => {
+    //     if (err) throw err;
+    // })
 
     let startTime = new Date();
     processAllBatches({docs: allQueries, from: 0, batchId: 0, batchSize: batchSize, esIndex: ES_INDEX_FINAL})
